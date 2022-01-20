9c304db417b2d9672900fd4d6bef50aa2c76ef68
unknown <Sophia Nguyen>
2022-01-19 19:31:22 -0600

    setting up database with sequelize

--- data/models/bootcamp.js
+++ data/models/bootcamp.js
@@ -1,4 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
+const { Sequelize } = require('../../../rate-my-bootcamp/config/connection');
+const db = require('../db/schema.sql');
+
 class Bootcamp {
     constructor(name, id) {
         this.id = id;
@@ -13,5 +16,16 @@ class Bootcamp {
     getId() {
         return this.id
     }
-    
+
+    const Bootcamp = db.define('bootcamp' {
+        instructor: {
+            type: Sequelize.STRING
+        },
+        feedback: {
+            type: Sequelize.STRING
+        },
+        user: {
+            type: Sequelize.STRING
+        },
+    })
     module.exports = Bootcamp;

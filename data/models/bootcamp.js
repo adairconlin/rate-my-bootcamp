c514ee16f2ed5205effdb0189c16eefeb3cc27ba
unknown <Sophia Nguyen>
2022-01-19 20:56:13 -0600

    updated bootcamp.js

--- data/models/bootcamp.js
+++ data/models/bootcamp.js
@@ -2,21 +2,6 @@ const { Model, DataTypes } = require('sequelize');
 const { Sequelize } = require('../../../rate-my-bootcamp/config/connection');
 const db = require('../db/schema.sql');
 
-class Bootcamp {
-    constructor(name, id) {
-        this.id = id;
-        this.name = name;
-    }
-}
-
-    getName() {
-        return this.name
-    }
-
-    getId() {
-        return this.id
-    }
-
     const Bootcamp = db.define('bootcamp' {
         instructor: {
             id: {

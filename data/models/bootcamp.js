752008f31d8a7227f1eb7e9560d2171766e4d66a
unknown <Sophia Nguyen>
2022-01-19 21:33:38 -0600

    updated bootcamp.js model

--- data/models/bootcamp.js
+++ data/models/bootcamp.js
@@ -4,78 +4,17 @@ const db = require('../db/schema.sql');
 
 class Bootcamp extends Model {}
 
-    const Bootcamp = db.define('bootcamp' {
-        instructor: {
-            id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                primarykey: true,
-                autoIncrement: true
-            },
-            name: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            bootcamp: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            }
+Bootcamp.init( 
+    {
+        id: {
+            primarykey: true,
+            autoIncrement: true,
         },
-        feedback: {
-            id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                primarykey: true,
-                autoIncrement: true
-            },
-            review_text: {
-                type: DataTypes.STRING,
-                allowNull: false
-                validate: {
-                    len: [75]
-                }
-            },
-            instructor_id: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            bootcamp_id: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            user_id: {
-                allowNull: false,
-    
-            },
-            rating: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            },
-        user: {
-            id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                primarykey: true,
-                autoIncrement: true
-            },
-            username: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            email: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                validate: {
-                    isEmail: true
-                }
-            },
-            password: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                validate: {
-                    len:[8]
-                }
-            }
-        },
-    })
-    module.exports = Bootcamp;
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }
+)
+
+module.exports = Bootcamp;

20d2ad7b42736ee775c70d59bc118ff6600b82fa
unknown <Sophia Nguyen>
2022-01-19 20:08:31 -0600

    creating tables database

--- data/db/schema.sql
+++ data/db/schema.sql
@@ -1,2 +1,31 @@
 -- create database
 bootcamp_db;
+
+CREATE TABLE bootcamp (
+    bootcamp_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    BootcampID STRING NOT NULL,
+    BootcampName STRING NOT NULL
+);
+
+
+CREATE TABLE instructor (
+    instructor_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    instructorName STRING NOT NULL,
+    bootcamp_id INTEGER NOT NULL
+);
+
+CREATE TABLE feedback (
+    feedback_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    review_text 
+    instructor_id NULL
+    bootcamp_id NOT NULL
+    user_id NOT NULL
+    rating NOT NULL
+);
+
+CREATE TABLE user (
+    user_id AUTO_INCREMENT PRIMARY KEY
+    username NOT NULL
+    email BOOLEAN
+    password VARCHAR(8) NOT NULL
+);

f253a09c34bf7cebf338287d88a26c0ef0502208
Max W <mwinter@wesleyan.edu>
2022-01-21 12:21:06 -0600

    adding home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,15 +11,69 @@ router.get("/login", (req, res) => {
 });
 
 router.get("/bootcamps", (req, res) => {
+    Bootcamp.findAll({
+        attributes: ["id", "name"],
+    })
+    .then(dbBootcampData => {
+        const bootcamps = dbBootcampData.map(bootcamp => bootcamp.get({ plain: true }));
 
+        res.render('bootcamps', { bootcamps, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        console.log(err);
+        res.json(500).json(err);
+    });
 });
 
 router.get("/bootcamp/:id", (req, res) => {
+    Bootcamp.findOne({
+        attributes: ["id", "name"],
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Feedback,
+                attributes: ["id", "review_text", "rating"],
+                include: {
+                    model: User,
+                    attributes: ["id", "username"]
+                }
+            },
+            {
+                model: Instructor,
+                attributes: ["id", "name"]
+            }
+        ]
+    })
+    .then(dbBootcampData => {
+        if (!dbBootcampData) {
+            res.status(404).json({ message: "No bootcamp found with this id." });
+            return;
+        }
+        const bootcamp = dbBootcampData.get({ plain: true });
 
+        res.render('single-bootcamp', { bootcamp, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        console.log(err);
+        res.json(500).json(err);
+    });
 });
 
 router.get("/instructors", (req, res) => {
+    Instructor.findAll({
+        attributes: ["id", "name", "bootcamp_id"]
+    })
+    .then(dbInstructorData => {
+        const instructors = dbInstructorData.map(instructor => instructor.get({ plain: true }));
 
+        res.render('instructors', { instructors, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
 });
 
 router.get("/instructor/:id", (req, res) => {

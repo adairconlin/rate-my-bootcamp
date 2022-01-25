e49843d5b75ad063b1b2cadb4678e469a9d1e44c
adairconlin <adair.conlin@gmail.com>
2022-01-25 11:34:36 -0600

    Fix POST routes for add-bootcamp.js

--- public/javascript/add-bootcamp.js
+++ public/javascript/add-bootcamp.js
@@ -2,43 +2,34 @@ async function addBootcamp(event) {
     event.preventDefault();
 
     const name = document.querySelector("#name").value;
-    const rating = document.querySelector("#rating").value;
-    const review_text = document.querySelector("#review").value;
 
-    const createBootcamp = await fetch("api/bootcamps", {
+    await fetch("api/bootcamps", {
         method: "POST",
         body: JSON.stringify({ name }),
-        headers: {
-            "Content-type": "application/json"
-        }
+        headers: { "Content-type": "application/json" }
     })
-    .then(bootcampData => {
-        const searchBootcamp = await fetch ("api/bootcamps", {
-            method: "GET"
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            addFeedback(data);
         })
-    });
-
-/*     if (!bootcampResponse.ok) {
-        alert(bootcampResponse.statusText);
-    }
-
-    console.log("Bootcamp response: " + bootcampResponse.dataValues.id);
-    const bootcamp_id = bootcampResponse.dataValues.id;
-    const user_id = req.body.user_id;
-    const feedbackResponse = await fetch("api/feedback", {
-        method: "POST",
-        body: JSON.stringify({ rating, review_text, bootcamp_id, user_id }),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
-
-    if (feedbackResponse.ok) {
-        window.location.replace('/dashboard');
-    } else {
-        alert(feedbackResponse.statusText);
-    } */
 };
 
+async function addFeedback(data) {
+    const rating = document.querySelector("#rating").value;
+    const review_text = document.querySelector("#review").value;
+    const bootcamp_id = data.id;
+    const user_id = "1";
+
+    await fetch("api/feedback", {
+        method: "POST",
+        body: JSON.stringify({ rating, review_text, bootcamp_id, user_id }),
+        headers: { "Content-type": "application/json" }
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        })
+}
 
 document.querySelector(".add-feedback").addEventListener("submit", addBootcamp);
